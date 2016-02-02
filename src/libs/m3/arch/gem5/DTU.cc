38dd84013a5f9145139a212b13e78f3bdee82098
Nils Asmussen <nils@os.inf.tu-dresden.de>
2016-02-02 10:06:26 +0100

    Revert "gem5: changed EP register layout."
    This reverts commit 1f4986234eb16a6c202fe547ff587e8ed8b5c272.

--- src/libs/m3/arch/gem5/DTU.cc
+++ src/libs/m3/arch/gem5/DTU.cc
@@ -26,37 +26,43 @@ void DTU::send(int ep, const void *msg, size_t size, label_t replylbl, int reply
     static_assert(MemGate::R == DTU::R, "DTU::R does not match MemGate::R");
     static_assert(MemGate::W == DTU::W, "DTU::W does not match MemGate::W");
 
-    write_reg(CmdRegs::DATA_ADDR, reinterpret_cast<uintptr_t>(msg));
-    write_reg(CmdRegs::DATA_SIZE, size);
-    write_reg(CmdRegs::REPLY_LABEL, replylbl);
-    write_reg(CmdRegs::REPLY_EP, reply_ep);
+    CmdRegs *c = cmd_regs();
+    c->dataAddr = reinterpret_cast<uintptr_t>(msg);
+    c->dataSize = size;
+    c->replyLabel = replylbl;
+    c->replyEpId = reply_ep;
     Sync::compiler_barrier();
-    write_reg(CmdRegs::COMMAND, buildCommand(ep, CmdOpCode::SEND));
+    c->command = buildCommand(ep, CmdOpCode::SEND);
 }
 
 void DTU::reply(int ep, const void *msg, size_t size, size_t) {
-    write_reg(CmdRegs::DATA_ADDR, reinterpret_cast<uintptr_t>(msg));
-    write_reg(CmdRegs::DATA_SIZE, size);
+    CmdRegs *c = cmd_regs();
+    c->dataAddr = reinterpret_cast<uintptr_t>(msg);
+    c->dataSize = size;
     Sync::compiler_barrier();
-    write_reg(CmdRegs::COMMAND, buildCommand(ep, CmdOpCode::REPLY));
+    c->command = buildCommand(ep, CmdOpCode::REPLY);
 }
 
 void DTU::read(int ep, void *msg, size_t size, size_t off) {
-    write_reg(CmdRegs::DATA_ADDR, reinterpret_cast<uintptr_t>(msg));
-    write_reg(CmdRegs::DATA_SIZE, size);
-    write_reg(CmdRegs::OFFSET, off);
+    CmdRegs *c = cmd_regs();
+
+    c->dataAddr = reinterpret_cast<uintptr_t>(msg);
+    c->dataSize = size;
+    c->offset = off;
     Sync::compiler_barrier();
-    write_reg(CmdRegs::COMMAND, buildCommand(ep, CmdOpCode::READ));
+    c->command = buildCommand(ep, CmdOpCode::READ);
 
     wait_until_ready(ep);
 }
 
 void DTU::write(int ep, const void *msg, size_t size, size_t off) {
-    write_reg(CmdRegs::DATA_ADDR, reinterpret_cast<uintptr_t>(msg));
-    write_reg(CmdRegs::DATA_SIZE, size);
-    write_reg(CmdRegs::OFFSET, off);
+    CmdRegs *c = cmd_regs();
+
+    c->dataAddr = reinterpret_cast<uintptr_t>(msg);
+    c->dataSize = size;
+    c->offset = off;
     Sync::compiler_barrier();
-    write_reg(CmdRegs::COMMAND, buildCommand(ep, CmdOpCode::WRITE));
+    c->command = buildCommand(ep, CmdOpCode::WRITE);
 
     wait_until_ready(ep);
 }
